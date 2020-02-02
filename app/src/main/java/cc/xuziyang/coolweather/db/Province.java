f0d279282c31670853f8dba51ae503e9b972c9c4
FelliYang <2303633806@qq.com>
2020-02-02 08:13:50 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/cc/xuziyang/coolweather/db/Province.java
@@ -0,0 +1,35 @@
+package cc.xuziyang.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/*
+* 创建三个数据库表 分别存储省、市、县*/
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
